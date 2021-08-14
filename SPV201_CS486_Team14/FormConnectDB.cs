427bd4c7dd7035d61056710d826b753399d55832
TruongQuyHai <73277864+Truong-Quy-Hai@users.noreply.github.com>
2021-08-14 10:27:17 +0700

    Form Connect

---
+++ SPV201_CS486_Team14/FormConnectDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroductionScreen
+{
+  public partial class FormConnectDB : Form
+  {
+    public FormConnectDB()
+    {
+      InitializeComponent();
+    }
+
+    private void connectBtn_Click(object sender, EventArgs e)
+    {
+      connectBtn.Image = global::SPV201_CS486_Team14.Properties.Resources.loading;
+    }
+  }
+}
