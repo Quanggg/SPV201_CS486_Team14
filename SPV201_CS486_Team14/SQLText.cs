e9ca561698efe941a25bed8c626c3a0e6e084944
vkduy19 <vkduy19@apcs.vn>
2021-08-14 11:29:35 +0700

    func call

--- SPV201_CS486_Team14/SQLText.cs
+++ SPV201_CS486_Team14/SQLText.cs
@@ -8,9 +8,9 @@ namespace SPV201_CS486_Team14
 {
     class SQLText
     {
-        public string DisplayContestants = "fn_displayContestant()";
-        public string DisplayExaminers = "fn_displayExaminer()";
-        public string DisplayOfficialMembers = "fn_displayOfficialMember()";
-        public string DisplayReserveMembers = "fn_displayReserveMember()";
+        public string DisplayContestants = "select * from dbo.fn_displayContestant()";
+        public string DisplayExaminers = "select * from dbo.fn_displayExaminer()";
+        public string DisplayOfficialMembers = "select * from dbo.fn_displayOfficialMember()";
+        public string DisplayReserveMembers = "select * from dbo.fn_displayReserveMember()";
     }
 }
