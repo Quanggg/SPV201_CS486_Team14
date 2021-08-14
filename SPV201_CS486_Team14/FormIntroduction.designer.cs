5c5c931b75c2f03e47c4e0b04589f827823c6cea
Quang Lê <quanggr8@gmail.com>
2021-08-14 10:34:16 +0700

    link introduction form

--- SPV201_CS486_Team14/FormIntroduction.designer.cs
+++ SPV201_CS486_Team14/FormIntroduction.designer.cs
@@ -29,176 +29,170 @@ namespace IntroductionScreen
     /// </summary>
     private void InitializeComponent()
     {
-      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormIntroduction));
-      this.pictureBox1 = new System.Windows.Forms.PictureBox();
-      this.pictureBox2 = new System.Windows.Forms.PictureBox();
-      this.pictureBox3 = new System.Windows.Forms.PictureBox();
-      this.pictureBox4 = new System.Windows.Forms.PictureBox();
-      this.pictureBox5 = new System.Windows.Forms.PictureBox();
-      this.pictureBox6 = new System.Windows.Forms.PictureBox();
-      this.pictureBox7 = new System.Windows.Forms.PictureBox();
-      this.pictureBox8 = new System.Windows.Forms.PictureBox();
-      this.pictureBox9 = new System.Windows.Forms.PictureBox();
-      this.pictureBox10 = new System.Windows.Forms.PictureBox();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
-      this.SuspendLayout();
-      // 
-      // pictureBox1
-      // 
-      this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
-      this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-      this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-      this.pictureBox1.Name = "pictureBox1";
-      this.pictureBox1.Size = new System.Drawing.Size(666, 134);
-      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-      this.pictureBox1.TabIndex = 0;
-      this.pictureBox1.TabStop = false;
-      // 
-      // pictureBox2
-      // 
-      this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-      this.pictureBox2.Location = new System.Drawing.Point(25, 719);
-      this.pictureBox2.Margin = new System.Windows.Forms.Padding(16, 48, 16, 0);
-      this.pictureBox2.Name = "pictureBox2";
-      this.pictureBox2.Size = new System.Drawing.Size(616, 305);
-      this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-      this.pictureBox2.TabIndex = 1;
-      this.pictureBox2.TabStop = false;
-      // 
-      // pictureBox3
-      // 
-      this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-      this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-      this.pictureBox3.Location = new System.Drawing.Point(56, 1080);
-      this.pictureBox3.Margin = new System.Windows.Forms.Padding(16, 56, 16, 0);
-      this.pictureBox3.Name = "pictureBox3";
-      this.pictureBox3.Size = new System.Drawing.Size(576, 267);
-      this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-      this.pictureBox3.TabIndex = 2;
-      this.pictureBox3.TabStop = false;
-      // 
-      // pictureBox4
-      // 
-      this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-      this.pictureBox4.Location = new System.Drawing.Point(25, 1395);
-      this.pictureBox4.Margin = new System.Windows.Forms.Padding(16, 48, 16, 0);
-      this.pictureBox4.Name = "pictureBox4";
-      this.pictureBox4.Size = new System.Drawing.Size(576, 271);
-      this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-      this.pictureBox4.TabIndex = 3;
-      this.pictureBox4.TabStop = false;
-      // 
-      // pictureBox5
-      // 
-      this.pictureBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-      this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-      this.pictureBox5.Location = new System.Drawing.Point(65, 1722);
-      this.pictureBox5.Margin = new System.Windows.Forms.Padding(16, 56, 16, 0);
-      this.pictureBox5.Name = "pictureBox5";
-      this.pictureBox5.Size = new System.Drawing.Size(576, 262);
-      this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-      this.pictureBox5.TabIndex = 4;
-      this.pictureBox5.TabStop = false;
-      // 
-      // pictureBox6
-      // 
-      this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-      this.pictureBox6.Location = new System.Drawing.Point(25, 2032);
-      this.pictureBox6.Margin = new System.Windows.Forms.Padding(16, 48, 16, 0);
-      this.pictureBox6.Name = "pictureBox6";
-      this.pictureBox6.Size = new System.Drawing.Size(576, 253);
-      this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-      this.pictureBox6.TabIndex = 5;
-      this.pictureBox6.TabStop = false;
-      // 
-      // pictureBox7
-      // 
-      this.pictureBox7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1049, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(14, 411);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(9, 27, 9, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(352, 174);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox3.Location = new System.Drawing.Point(679, 617);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(9, 32, 9, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(329, 153);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(14, 797);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(9, 27, 9, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(329, 155);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox5.Location = new System.Drawing.Point(684, 984);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(9, 32, 9, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(329, 150);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(14, 1161);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(9, 27, 9, 0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(329, 145);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
-      this.pictureBox7.Location = new System.Drawing.Point(63, 2341);
-      this.pictureBox7.Margin = new System.Windows.Forms.Padding(16, 56, 16, 0);
-      this.pictureBox7.Name = "pictureBox7";
-      this.pictureBox7.Size = new System.Drawing.Size(556, 511);
-      this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-      this.pictureBox7.TabIndex = 6;
-      this.pictureBox7.TabStop = false;
-      // 
-      // pictureBox8
-      // 
-      this.pictureBox8.Dock = System.Windows.Forms.DockStyle.Top;
-      this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
-      this.pictureBox8.Location = new System.Drawing.Point(0, 134);
-      this.pictureBox8.Name = "pictureBox8";
-      this.pictureBox8.Size = new System.Drawing.Size(666, 50);
-      this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-      this.pictureBox8.TabIndex = 7;
-      this.pictureBox8.TabStop = false;
-      // 
-      // pictureBox9
-      // 
-      this.pictureBox9.Dock = System.Windows.Forms.DockStyle.Top;
-      this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
-      this.pictureBox9.Location = new System.Drawing.Point(0, 184);
-      this.pictureBox9.Name = "pictureBox9";
-      this.pictureBox9.Size = new System.Drawing.Size(666, 225);
-      this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-      this.pictureBox9.TabIndex = 8;
-      this.pictureBox9.TabStop = false;
-      // 
-      // pictureBox10
-      // 
-      this.pictureBox10.Dock = System.Windows.Forms.DockStyle.Top;
-      this.pictureBox10.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox10.Image")));
-      this.pictureBox10.Location = new System.Drawing.Point(0, 409);
-      this.pictureBox10.Name = "pictureBox10";
-      this.pictureBox10.Size = new System.Drawing.Size(666, 259);
-      this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-      this.pictureBox10.TabIndex = 9;
-      this.pictureBox10.TabStop = false;
-      // 
-      // FormIntroduction
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(168F, 168F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-      this.AutoScroll = true;
-      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(49)))));
-      this.ClientSize = new System.Drawing.Size(696, 448);
-      this.Controls.Add(this.pictureBox10);
-      this.Controls.Add(this.pictureBox9);
-      this.Controls.Add(this.pictureBox8);
-      this.Controls.Add(this.pictureBox7);
-      this.Controls.Add(this.pictureBox6);
-      this.Controls.Add(this.pictureBox5);
-      this.Controls.Add(this.pictureBox4);
-      this.Controls.Add(this.pictureBox3);
-      this.Controls.Add(this.pictureBox2);
-      this.Controls.Add(this.pictureBox1);
-      this.MinimumSize = new System.Drawing.Size(720, 512);
-      this.Name = "FormIntroduction";
-      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-      this.Text = "Team 14 - Introduction";
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.pictureBox7.Location = new System.Drawing.Point(36, 1338);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(9, 32, 9, 0);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(965, 292);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox8.Location = new System.Drawing.Point(0, 134);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(1049, 29);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox9.Location = new System.Drawing.Point(0, 163);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(1049, 129);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox10.Location = new System.Drawing.Point(0, 292);
+            this.pictureBox10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(1049, 148);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            // 
+            // FormIntroduction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(1066, 585);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimumSize = new System.Drawing.Size(418, 309);
+            this.Name = "FormIntroduction";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Team 14 - Introduction";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
